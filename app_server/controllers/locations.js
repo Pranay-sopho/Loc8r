16a05072af28164e11866109415881467769f4da
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-07-18 16:15:27 -0700

    Data moved from views(locationinfo and add-review) to controller.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -13,23 +13,64 @@ module.exports.homelist = function(req, res){
             rating: 3,
             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '100m'
-        }, {
+        },{
             name: 'Starcups',
             address: '125 High Street, Reading, RG6 1PS',
             rating: 3,
             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '100m'
-        }
-        ]
+        }]
     });
 };
 
 /* GET Location Info page */
 module.exports.locationInfo = function(req, res){
-    res.render('location-info', { title: 'Location Info' });
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {title: 'Starcups'},
+        sidebar: {
+            context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {lat: 51.455041, lng: -0.9690884},
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            },{
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            },{
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Pranay Kotkar',
+                rating: 5,
+                timestamp: '18 July 2017',
+                reviewText: 'What a great place. I can\'t say enough good things about.'
+            },{
+                author: 'Pranay Kotkar',
+                rating: 5,
+                timestamp: '18 July 2017',
+                reviewText: 'What a great place. I can\'t say enough good things about.'
+            }]
+        }
+    });
 };
 
 /* GET Add Review page */
 module.exports.addReview = function(req, res){
-    res.render('location-review-form', { title: 'Add Review' });
+    res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: {title: 'Review Starcups'}
+    });
 };
