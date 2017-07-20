ba6a9f798e9c92060d5f7f704f2838cf8bdd3409
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-07-20 13:48:07 -0700

    Settings for mongodb connection and disconnection(close on app termination)

---
+++ app_server/models/db.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+
+var dbURL = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURL, {useMongoClient: true});
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to', dbURL);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose Connection Error:', err);
+});
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose Disconnected');
+});
+
+gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through', msg);
+        callback();
+    });
+};
+
+// For App Termination
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+// For Heroku App Termination
+process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
