afb0ac66e4d6a9297c3005d6d586b8ffc9cc8252
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-07-21 01:01:14 -0700

    api controllers added and models moved from app_server to app_api

---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+module.exports.reviewsCreate = function (req, res) {
+    sendJsonResponse(res, 200, {"status": "success"});
+};
+
+module.exports.reviewsReadOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status": "success"});
+};
+
+module.exports.reviewsUpdateOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status": "success"});
+};
+
+module.exports.reviewsDeleteOne = function (req, res) {
+    sendJsonResponse(res, 200, {"status": "success"});
+};
