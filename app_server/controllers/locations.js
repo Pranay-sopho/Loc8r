4775794fdd3bbbad798ce0a1f2326e37ef1c76fb
Pranay Kotkar <kotkar.pranay04@gmail.com>
2017-07-27 22:52:01 -0700

    details page retrieving and showing data received from api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -66,46 +66,53 @@ module.exports.homelist = function(req, res){
     });
 };
 
-/* GET Location Info page */
-module.exports.locationInfo = function(req, res){
+var renderDetailPage = function (req, res, locDetail) {
     res.render('location-info', {
-        title: 'Starcups',
-        pageHeader: {title: 'Starcups'},
+        title: locDetail.name,
+        pageHeader: {title: locDetail.name},
         sidebar: {
             context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
             callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
         },
-        location: {
-            name: 'Starcups',
-            address: '125 High Street, Reading, RG6 1PS',
-            rating: 3,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-            coords: {lat: 51.455041, lng: -0.9690884},
-            openingTimes: [{
-                days: 'Monday - Friday',
-                opening: '7:00am',
-                closing: '7:00pm',
-                closed: false
-            },{
-                days: 'Saturday',
-                opening: '8:00am',
-                closing: '5:00pm',
-                closed: false
-            },{
-                days: 'Sunday',
-                closed: true
-            }],
-            reviews: [{
-                author: 'Pranay Kotkar',
-                rating: 5,
-                timestamp: '18 July 2017',
-                reviewText: 'What a great place. I can\'t say enough good things about.'
-            },{
-                author: 'Pranay Kotkar',
-                rating: 5,
-                timestamp: '18 July 2017',
-                reviewText: 'What a great place. I can\'t say enough good things about.'
-            }]
+        location: locDetail
+    });
+};
+
+var _showError = function (req, res, status) {
+    var title, content;
+    if(status === 404){
+        title = "404, page not found";
+        content = "Oh dear. Looks like we can't find this page. Sorry.";
+    } else {
+        title = status + ", something's gone wrong";
+        content = "Something, somewhere, has gone just a little bit wrong.";
+    }
+    res.status(status);
+    res.render('generic-text', {
+        title: title,
+        content: content
+    });
+};
+
+/* GET Location Info page */
+module.exports.locationInfo = function(req, res){
+    var requestOptions, path;
+    path = "/api/locations/" + req.params.locationid;
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {}
+    };
+    request(requestOptions, function (err, response, body) {
+        var data = body;
+        if(response.statusCode === 200){
+            data.coords = {
+                lng: body.coords[0],
+                lat: body.coords[1]
+            };
+            renderDetailPage(req, res, data);
+        } else {
+            _showError(req, res, response.statusCode);
         }
     });
 };
